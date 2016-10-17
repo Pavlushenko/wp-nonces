f7ccb20ad0f95a377cae36c710001e2dd56bc30a
anjela <anjela_pavlushenko@mail.ru>
2016-10-17 19:08:02 +0300

    a composer package which serves the functionality working with WordPress Nonces in an object orientated environment

--- tests/WPNonceTest.php
+++ tests/WPNonceTest.php
@@ -1,14 +1,87 @@
 <?php
-//require_once 'PHPUnit/Framework.php';
-use PHPUnit\Framework\TestCase;
 
-//class WP_Nonce_OOP_Test extends PHPUnit_Framework_TestCase {
-class WPNonceTest extends TestCase {
+use Pavlushenko\WPNonce;
+
+class WPNonceTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_wp_nonce_ays_oop($action='log-out') {
+        $WPNonce = new WPNonce();
+        $nonceAys = $WPNonce -> wp_nonce_ays_oop($action='log-out');
+        $this->assertEquals(1, $nonceAys);
+    }
+
+    public function test_wp_nonce_field_oop() {
+        $WPNonce = new WPNonce();
+        $nonceField = $WPNonce -> wp_nonce_field_oop();
+        $this->assertEquals(1, $nonceField);
+    }
+
+    public function test_wp_nonce_url_oop($actionurl='http://site.ru/url') {
+        $WPNonce = new WPNonce();
+        $nonceUrl = $WPNonce -> wp_nonce_url_oop($actionurl='http://site.ru/url');
+        $this->assertEquals(1, $nonceUrl);
+    }
+
+    public function test_wp_verify_nonce_oop($nonce='my-nonce') {
+        $WPNonce = new WPNonce();
+        $nonceVerify = $WPNonce -> wp_verify_nonce_oop($nonce='my-nonce');
+        $this->assertEquals(1, $nonceVerify);
+    }
 
     public function test_wp_create_nonce_oop() {
-        $nonceCreate = wp_create_nonce();
-        $WP_Nonce_OOP = new WP_Nonce_OOP();
-        $nonceCreateOop = $WP_Nonce_OOP -> wp_create_nonce_oop();
-        $this->assertEquals($nonceCreate, $nonceCreateOop);
+        $WPNonce = new WPNonce();
+        $nonceCreate = $WPNonce -> wp_create_nonce_oop();
+        $this->assertEquals(1, $nonceCreate);
     }
+
+    public function test_check_admin_referer_oop() {
+        $WPNonce = new WPNonce();
+        $checkAdminRefer = $WPNonce -> check_admin_referer_oop();
+        $this->assertEquals(1, $checkAdminRefer);
+    }
+
+    public function test_check_ajax_referer_oop() {
+        $WPNonce = new WPNonce();
+        $checkAjaxRefer = $WPNonce -> check_ajax_referer_oop();
+        $this->assertEquals(1, $checkAjaxRefer);
+    }
+
+    public function test_wp_referer_field_oop() {
+        $WPNonce = new WPNonce();
+        $wpReferField = $WPNonce -> wp_referer_field_oop();
+        $this->assertEquals(1, $wpReferField);
+    }
+
+}
+
+function wp_nonce_ays($action='log-out') {
+    return 1;
+}
+
+function wp_nonce_field() {
+    return 1;
+}
+
+function wp_nonce_url($actionurl='http://site.ru/url') {
+    return 1;
+}
+
+function wp_verify_nonce($nonce='my-nonce') {
+    return 1;
+}
+
+function wp_create_nonce() {
+    return 1;
+}
+
+function check_admin_referer() {
+    return 1;
+}
+
+function check_ajax_referer() {
+    return 1;
+}
+
+function wp_referer_field() {
+    return 1;
 }
